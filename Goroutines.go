a6a6e21c1f5ae06dd21f9c5f9f71b6e59d2485ea
Xingyan Li <lxy961128y@gmail.com>
2019-09-05 22:38:05 -0400

    Goroutines Added

--- Goroutines.go
+++ Goroutines.go
@@ -1,18 +1,66 @@
 package main
 import (
 	"fmt"
+	"runtime"
+	"sync"
+	//"time"
 )
 /**
 Creating goroutines
 Synchronization(WaitGroups, Mutexes)
 Parallelism
-Best practices
+
+Best practices:
+	Don't create goroutines in libraries
+	Let consumer control concurrency
+	When creating a goroutine, know how it will end.
+		Avoid subtle memory leaks
+	Check for race conditions at compile time
  */
 
+ var wg = sync.WaitGroup{}
+ var counter = 0
+ var m = sync.RWMutex{}
+
  func main() {
- 	sayHello()
+ 	/** Example 1
+ 	var msg = "Hello"
+ 	go func(msg string) {  // pass the variable in case it gets modified
+ 		fmt.Println(msg)
+	}(msg)
+ 	msg = "Goodbye"
+ 	time.Sleep(100 * time.Millisecond)  // this is a bad practice
+ 	*/
+
+ 	/** Example 2
+	 var msg = "Hello"
+	 wg.Add(1)
+	 go func(msg string) {  // pass the variable in case it gets modified
+		 fmt.Println(msg)
+		 wg.Done()
+	 }(msg)
+	 msg = "Goodbye"
+	 wg.Wait()
+ 	*/
+
+ 	runtime.GOMAXPROCS(100)
+ 	for i := 0; i < 10; i++ {
+ 		wg.Add(2)
+ 		m.RLock()
+ 		go sayHello()
+ 		m.Lock()
+ 		go increment()
+	}
+ 	wg.Wait()
  }
 
-func sayHello() {
-	fmt.Println("Hello")
-}
+ func sayHello() {
+ 	fmt.Printf("Hello #%v\n", counter)
+ 	m.RUnlock()
+	wg.Done()
+ }
+ func increment() {
+ 	counter++
+ 	m.Unlock()
+ 	wg.Done()
+ }
